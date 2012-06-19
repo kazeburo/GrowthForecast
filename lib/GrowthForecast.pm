584e76bff4e116cd75687cf349dd4542f5780c30
Masahiro Nagano <kazeburo@gmail.com>
2012-06-19 23:12:00 +0900

    renew Makefile.PL

--- lib/GrowthForecast.pm
+++ lib/GrowthForecast.pm
@@ -8,3 +8,28 @@ our $VERSION = 0.01;
 
 1;
 
+__END__
+
+=head1 NAME
+
+GrowthForecast - Lightning Fast Graphing/Visualization
+
+=head1 SYNOPSIS
+
+  % growthforecast.pl -h
+
+=head1 DESCRIPTION
+
+GrowthForecast is graphing/visualization web tool built on RRDTools
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
